4fe8fd6305954e654d0e043d0d1495d37a3b254f
RyanSeda <Ryan.Seda@barronsystem.com>
2014-06-02 11:42:09 -0400

    Removed Test Commit
    Did not mean to modify Copyright Statement during test commit. Removed.

--- FlyoutNavigation/FlyoutNavigationController.cs
+++ FlyoutNavigation/FlyoutNavigationController.cs
@@ -1,6 +1,4 @@
 //  Copyright 2011  Clancey
-// 
-//		TEST COMMIT RYANSEDA
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
